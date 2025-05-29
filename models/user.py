55a365d122a795b0d1b7fa6a15f3e26fb3d26d80
Puja K. Negi <puja.knegi2020@gmail.com>
2025-05-29 11:13:56 +0545

    Feat: Create User model

---
+++ models/user.py
@@ -0,0 +1,12 @@
+import uuid
+from sqlalchemy import Column, String
+from db.session import Base
+from sqlalchemy.dialects.postgresql import UUID
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
