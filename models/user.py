870d58de91553ec90455718d1f119b1c2d748fb7
Puja K. Negi <puja.knegi2020@gmail.com>
2025-05-29 11:37:02 +0545

    chore: modified user model

--- models/user.py
+++ models/user.py
@@ -1,12 +1,10 @@
-import uuid
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, DateTime
 from db.session import Base
 from sqlalchemy.dialects.postgresql import UUID
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    email = Column(String, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
