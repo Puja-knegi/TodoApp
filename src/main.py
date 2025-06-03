ce2c0d9a9444b9d495df598ab1e051428935907f
Puja K. Negi <puja.knegi2020@gmail.com>
2025-06-03 16:04:22 +0545

    feat: add user routes for fetching all users

--- src/main.py
+++ src/main.py
@@ -1,19 +1,18 @@
 from fastapi import FastAPI
 from src.authentications.routers import auth_router
+from src.users.routers import users_router
 from src.db.session import engine, Base
-
-version = "V1"
 
 app = FastAPI(
     title="User Authentication",
     description="API for a user authentication in TODO Application.",
-    version=version
 )
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
 
-app.include_router(auth_router, prefix=f"/api/{version}/auth", tags=['auth'])
+app.include_router(auth_router, prefix=f"/api/auth", tags=['auth'])
+app.include_router(users_router, prefix=f"/api/users", tags=['users'])  
 
 @app.get("/")
 def read_root():
